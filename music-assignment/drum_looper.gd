d749b27d45e3ea6e49ba3da503a5d5fc66b15664
skooter500 <bryan.duggan@tudublin.ie>
2024-12-03 11:24:07 +0000

    commented

--- music-assignment/drum_looper.gd
+++ music-assignment/drum_looper.gd
@@ -26,15 +26,27 @@ var num_samples:int
 var dir = 1
 
 func _on_timer_timeout1() -> void:
+	
+	# go forward and backward through the steps
+	
+	# get the value from the array
+	# the value will tell us whish sound to play
 	var sn = pattern[current]
+	
+	# -1 indicates dont play anything this step
 	if sn != -1:		
 		var s = get_child(sn)
 		s.play()
 		print(current)
+	
+	# go to the next step
 	current = current + dir
+	
+	# if we reach the last step, change direction and go back
 	if current == steps:
 		current = current - 1
 		dir = - dir
+	# if we reach the first step change direction and go forwards
 	if current == -1:
 		current = 0
 		dir = - dir
@@ -42,11 +54,16 @@ func _on_timer_timeout1() -> void:
 
 
 func _on_button_2_button_down() -> void:
+	# gets the child node, which is an AudioPlayer
+	# current is the index of node
 	var s = get_child(current)
 	s.play()
 	print(current)
+	
+	# add one to current so we go to the next one
 	current = current + 1
 	
+	# if we reach the last one, go back to the start
 	if current == steps:
 		current = 0
 	
